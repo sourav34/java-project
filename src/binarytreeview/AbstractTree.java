f329e81314620dc519702da5156393ffa4f6620e
ASUS <ASUS@asus-pc>
2016-01-30 23:23:14 +0600

    new commit

---
+++ src/binarytreeview/AbstractTree.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarytreeview;
+
+import sun.reflect.generics.tree.Tree;
+
+public abstract class AbstractTree<E>  {
+  /** Inorder traversal from the root*/
+  public void inorder() {
+  }
+
+  /** Postorder traversal from the root */
+  public void postorder() {
+  }
+
+  /** Preorder traversal from the root */
+  public void preorder() {
+  }
+
+  /** Return true if the tree is empty */
+  public boolean isEmpty() {
+      return true;
+    
+  }
+}
